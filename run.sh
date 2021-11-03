dd5f8a59be4619a0fe017f09325058483a766ccf
schroot72 <48331496+schroot72@users.noreply.github.com>
2021-11-03 20:46:09 +0100

    Update run.sh
    changed wwwroot to config so changes are lost when upgraded

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 
 [[ ! -e /config/scripts ]] && cp -R /src/domoticz/scripts /config/
 [[ ! -e /config/plugins ]] && cp -R /src/domoticz/plugins /config/
+[[ ! -e /config/www ]] && cp -R /src/domoticz/www /config/
 
 service mosquitto start
-/src/domoticz/domoticz -userdata /config/ -dbase /config/domoticz.db -sslwww 0
+/src/domoticz/domoticz -userdata /config/ -dbase /config/domoticz.db -wwwroot /config/www -sslwww 0
 #service domoticz.sh start
 #xtail /tmp/domoticz.txt
