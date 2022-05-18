c8685b2fa12c49c873926f9cd5e7b6d802ab17e4
schroot72 <48331496+schroot72@users.noreply.github.com>
2022-05-18 20:32:26 +0200

    Update run.sh
    improved script

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
-!/bin/bash
+#!/bin/bash
 
 
 [[ ! -e /config/scripts ]] && cp -R /src/domoticz/scripts /config/
 [[ ! -e /config/plugins ]] && cp -R /src/domoticz/plugins /config/
 [[ ! -e /config/www ]] && cp -R /src/domoticz/www /config/
 
-file="/src/docker-debian-domoticz.txt"
-version=$(cat "$file")
-if [ "$version" != "versie 1.3.0" ] then
- cp -RU /src/domoticz/www /config/
+file="/config/docker-debian-domoticz.txt"
+if [ -f "$file" ] 
+then
+ version=$(cat "$file")
+  if [ "$version" != "versie 1.3.0" ] 
+then
+    cp -Ru /src/domoticz/www /config/
+   cp /src/docker-debian-domoticz.txt /config/
+  fi
 fi
 
 service mosquitto start
 /src/domoticz/domoticz -userdata /config/ -dbase /config/domoticz.db -wwwroot /config/www -sslwww 0
-#service domoticz.sh start
-#xtail /tmp/domoticz.txt
