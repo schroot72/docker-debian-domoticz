6900a360b70a1875fb3412d50395660a56a90b7e
schroot72 <48331496+schroot72@users.noreply.github.com>
2022-05-18 19:59:27 +0200

    Update run.sh
    disabled SSL, check version installed and optionally update www files.

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@
 [[ ! -e /config/plugins ]] && cp -R /src/domoticz/plugins /config/
 [[ ! -e /config/www ]] && cp -R /src/domoticz/www /config/
 
+file="/src/docker-debian-domoticz.txt"
+version=$(cat "$file")
+if [ "$version" != "versie 1.3.0" ] then
+ cp -RU /src/domoticz/www /config/
+fi
+
 service mosquitto start
 /src/domoticz/domoticz -userdata /config/ -dbase /config/domoticz.db -wwwroot /config/www -sslwww 0
 #service domoticz.sh start
