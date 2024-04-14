d567a73afcbfb7345fe4325de0f94090cf54654c
schroot72 <48331496+schroot72@users.noreply.github.com>
2024-04-14 14:30:20 +0200

    Update run.sh
    revised for running with sh and bash

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# version 2022-05-19
+# version 2024-04-14
 
-[[ ! -e /config/scripts ]] && cp -R /src/domoticz/scripts /config/
-[[ ! -e /config/plugins ]] && cp -R /src/domoticz/plugins /config/
-[[ ! -e /config/www ]] && cp -R /src/domoticz/www /config/
+if [ -e /config/docker-debian-domoticz.txt ]
+then
+    echo "previous config found"
+else
+cp -R /src/domoticz/scripts /config/
+cp -R /src/domoticz/plugins /config/
+cp -R /src/domoticz/www /config/
+
+fi
 
 file="/config/docker-debian-domoticz.txt"
 newfile="/src/docker-debian-domoticz.txt"
@@ -13,10 +19,17 @@ then
  newversion=$(cat "$newfile")
   if [ "$version" != "$newversion" ] 
 then
+    # update config files with new versions
+echo "previous config updating ..."
     cp -Ru /src/domoticz/www /config/
-   cp /src/docker-debian-domoticz.txt /config/
+    cp -Ru /src/domoticz/plugins /config/
+    cp -Ru /src/domoticz/scripts /config/
+    cp /src/docker-debian-domoticz.txt /config/
   fi
 fi
 
 service mosquitto start
 /src/domoticz/domoticz -userdata /config/ -dbase /config/domoticz.db -wwwroot /config/www -sslwww 0
+
+
+    
